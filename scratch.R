a00673e65f7ffaece17c335a75d0de245c6175fd
Pete-Nelson <pnelson1@ucsc.edu>
2026-01-27 16:43:10 -0800

    some code for trying out stuff from one of the recent lectures

--- scratch.R
+++ scratch.R
@@ -13,3 +13,32 @@ US_energy.wide %>%
   head()
 
 file.choose()
+
+# functions ----
+# notes from 'Programming w Functions' lecture
+
+grouped_mean <- function(df, group_var, mean_var) {
+  df %>% # assume tidy version of the pipe works fine too?!
+    group_by({{group_var}}) %>% # why the double curly brackets? {{}}
+    summarise(mean({{mean_var}}))
+}
+
+# dbl curlies, I think, tell the function to confine that argument to the 
+# varible only. above, that'd limit it to working within 'group_by'...but 
+# that doesn't make complete sense to me
+
+library(tidyverse)
+# wctri_catch %>% grouped_mean(SPECIES_CODE, NUMBER_FISH)
+whales <- read_csv("data/whale_popn_wide.csv")
+whales
+
+whales.long <- whales %>% 
+  pivot_longer(3:5, 
+               names_to = "SPECIES_CODE",
+               values_to = "NUMBER_FISH") %>% 
+  print()
+
+whales.long[3,4] <- 40
+whales.long
+
+whales.long %>% grouped_mean('SPECIES_CODE', 'NUMBER_FISH')
